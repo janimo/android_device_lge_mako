af96b377c00571422a072a691960aaf9bb7121ad
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-01-09 16:17:46 -0200

    conn_init.c: Removing not used variable
    conn_init.c: Removing not used variable
    
    Change-Id: I5b631bc541f029da5ea3e1ce85755828d7b48afd
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- conn_init/conn_init.c
+++ conn_init/conn_init.c
@@ -30,7 +30,6 @@ static int wifi_check_qcom_cfg_files()
 
     // Read MAC String
     FILE *fp = NULL;
-    int n = 0;
     fp = fopen("/persist/wifi/.macaddr", "r");
     if ( fp == NULL )
     {
@@ -39,7 +38,7 @@ static int wifi_check_qcom_cfg_files()
     }
     else
     {
-        n = fread(macAddress, 12, 1, fp);
+        fread(macAddress, 12, 1, fp);
         fclose(fp);
 
         // Write MAC String
